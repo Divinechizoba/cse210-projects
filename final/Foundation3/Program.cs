96a63b5e902c599334b60a5a957e16661fd5cfb4
Divinechizoba <chu21012@byui.edu>
2023-12-14 04:44:09 +0100

    program 3 & 4

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,131 @@
 using System;
 
+// Base class for all event types
+class Event
+{
+    // Common event attributes
+    public string EventTitle { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public string Time { get; set; }
+    public string Address { get; set; }
+
+    // Method to generate standard event message
+    public virtual void GenerateStandardMessage()
+    {
+        Console.WriteLine($"Standard Event Details:");
+        Console.WriteLine($"Title: {EventTitle}");
+        Console.WriteLine($"Description: {Description}");
+        Console.WriteLine($"Date: {Date.ToShortDateString()}");
+        Console.WriteLine($"Time: {Time}");
+        Console.WriteLine($"Address: {Address}");
+    }
+
+    // Method to generate full event message
+    public virtual void GenerateFullMessage()
+    {
+        GenerateStandardMessage();
+    }
+
+    // Method to generate short event message
+    public virtual void GenerateShortMessage()
+    {
+        Console.WriteLine($"Short Event Details:");
+        Console.WriteLine($"Type: {GetType().Name}");
+        Console.WriteLine($"Title: {EventTitle}");
+        Console.WriteLine($"Date: {Date.ToShortDateString()}");
+    }
+}
+
+// Derived class for Lecture events
+class Lecture : Event
+{
+    // Additional attributes for lectures
+    public string Speaker { get; set; }
+    public int Capacity { get; set; }
+
+    // Override method to generate full event message for lectures
+    public override void GenerateFullMessage()
+    {
+        GenerateStandardMessage();
+        Console.WriteLine($"Event Type: Lecture");
+        Console.WriteLine($"Speaker: {Speaker}");
+        Console.WriteLine($"Capacity: {Capacity}");
+    }
+}
+
+// Derived class for Reception events
+class Reception : Event
+{
+    // Additional attribute for receptions
+    public string RSVPEmail { get; set; }
+
+    // Override method to generate full event message for receptions
+    public override void GenerateFullMessage()
+    {
+        GenerateStandardMessage();
+        Console.WriteLine($"Event Type: Reception");
+        Console.WriteLine($"RSVP Email: {RSVPEmail}");
+    }
+}
+
+// Derived class for Outdoor Gathering events
+class OutdoorGathering : Event
+{
+    // Additional attribute for outdoor gatherings
+    public string WeatherForecast { get; set; }
+
+    // Override method to generate full event message for outdoor gatherings
+    public override void GenerateFullMessage()
+    {
+        GenerateStandardMessage();
+        Console.WriteLine($"Event Type: Outdoor Gathering");
+        Console.WriteLine($"Weather Forecast: {WeatherForecast}");
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Creating instances of different event types
+        Lecture lectureEvent = new Lecture
+        {
+            EventTitle = "Programming Workshop",
+            Description = "A workshop on programming concepts",
+            Date = DateTime.Now.AddDays(7),
+            Time = "10:00 AM",
+            Address = "123 Main Street",
+            Speaker = "Dr. Smith",
+            Capacity = 50
+        };
+
+        Reception receptionEvent = new Reception
+        {
+            EventTitle = "Networking Event",
+            Description = "Networking event for professionals",
+            Date = DateTime.Now.AddDays(14),
+            Time = "6:00 PM",
+            Address = "456 Oak Avenue",
+            RSVPEmail = "info@example.com"
+        };
+
+        OutdoorGathering outdoorEvent = new OutdoorGathering
+        {
+            EventTitle = "Summer Picnic",
+            Description = "Picnic for employees and families",
+            Date = DateTime.Now.AddDays(21),
+            Time = "12:00 PM",
+            Address = "789 Park Lane",
+            WeatherForecast = "Sunny"
+        };
+
+        // Displaying event details
+        Console.WriteLine("Event Details:");
+        lectureEvent.GenerateFullMessage();
+        Console.WriteLine();
+        receptionEvent.GenerateFullMessage();
+        Console.WriteLine();
+        outdoorEvent.GenerateFullMessage();
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,142 @@
 using System;
+using System.Collections.Generic;
+
+// Base class for exercise activities
+class Activity
+{
+    // Common activity attributes
+    public DateTime Date { get; set; }
+    public int LengthInMinutes { get; set; }
+
+    // Method to calculate distance (to be overridden by derived classes)
+    public virtual double CalculateDistance()
+    {
+        return 0;
+    }
+
+    // Method to calculate speed (to be overridden by derived classes)
+    public virtual double CalculateSpeed()
+    {
+        return 0;
+    }
+
+    // Method to calculate pace (to be overridden by derived classes)
+    public virtual double CalculatePace()
+    {
+        return 0;
+    }
+
+    // Method to generate summary of the activity
+    public virtual void GetSummary()
+    {
+        Console.WriteLine($"{Date.ToShortDateString()} {GetType().Name} ({LengthInMinutes} min)");
+        Console.WriteLine($"Distance: {CalculateDistance()} miles");
+        Console.WriteLine($"Speed: {CalculateSpeed()} mph");
+        Console.WriteLine($"Pace: {CalculatePace()} min per mile");
+    }
+}
+
+// Derived class for Running activity
+class Running : Activity
+{
+    // Additional attribute for running
+    public double Distance { get; set; }
+
+    // Override method to calculate distance for running
+    public override double CalculateDistance()
+    {
+        return Distance;
+    }
+
+    // Override method to calculate speed for running
+    public override double CalculateSpeed()
+    {
+        return Distance / (LengthInMinutes / 60.0);
+    }
+
+    // Override method to calculate pace for running
+    public override double CalculatePace()
+    {
+        return LengthInMinutes / Distance;
+    }
+}
+
+// Derived class for Cycling activity
+class Cycling : Activity
+{
+    // Additional attribute for cycling
+    public double Speed { get; set; }
+
+    // Override method to calculate distance for cycling
+    public override double CalculateDistance()
+    {
+        return Speed * (LengthInMinutes / 60.0);
+    }
+
+    // Override method to calculate pace for cycling
+    public override double CalculatePace()
+    {
+        return 60.0 / Speed;
+    }
+}
+
+// Derived class for Swimming activity
+class Swimming : Activity
+{
+    // Additional attribute for swimming
+    public int Laps { get; set; }
+
+    // Override method to calculate distance for swimming
+    public override double CalculateDistance()
+    {
+        return Laps * 0.03125; // 1 lap is approximately 50 meters (converted to miles)
+    }
+
+    // Override method to calculate speed for swimming
+    public override double CalculateSpeed()
+    {
+        return CalculateDistance() / (LengthInMinutes / 60.0);
+    }
+
+    // Override method to calculate pace for swimming
+    public override double CalculatePace()
+    {
+        return LengthInMinutes / CalculateDistance();
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Creating instances of different exercise activities
+        Running runningActivity = new Running
+        {
+            Date = DateTime.Now,
+            LengthInMinutes = 30,
+            Distance = 3.5 // in miles
+        };
+
+        Cycling cyclingActivity = new Cycling
+        {
+            Date = DateTime.Now.AddDays(-1),
+            LengthInMinutes = 45,
+            Speed = 15 // in mph
+        };
+
+        Swimming swimmingActivity = new Swimming
+        {
+            Date = DateTime.Now.AddDays(-2),
+            LengthInMinutes = 60,
+            Laps = 20
+        };
+
+        // Displaying activity summaries
+        Console.WriteLine("Exercise Activity Summaries:");
+        runningActivity.GetSummary();
+        Console.WriteLine();
+        cyclingActivity.GetSummary();
+        Console.WriteLine();
+        swimmingActivity.GetSummary();
     }
 }
